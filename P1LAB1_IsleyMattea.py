656d8600c7f69c9761a88a1493f790a6754a19e4
isleym9447 <150169210+isleym9447@users.noreply.github.com>
2023-11-07 11:21:53 -0500

    First upload :)

---
+++ P1LAB1_IsleyMattea.py
@@ -0,0 +1,9 @@
+#Mattea Isley
+#11/7/2023
+#Basic Input/Output
+
+#Get input from the user
+name = input("Please enter your name: ")
+
+#Display output to the user
+print("Hello", name, "welcome to cti-110!")
