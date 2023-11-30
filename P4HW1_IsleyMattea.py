4fb140d610578e0fdd9894c9c6db7720178b4ddf
isleym9447 <150169210+isleym9447@users.noreply.github.com>
2023-11-30 10:45:20 -0500

    P1HW1 Updated homework content FINAL

--- P4HW1_IsleyMattea.py
+++ P4HW1_IsleyMattea.py
@@ -15,33 +15,17 @@ grades_list = []
 #Get grades from the user
 for i in range(num_grades):
     grade = float(input(f"Enter grade for module {i + 1}: "))
+#Grade is invalid if less than 0 or more than 100
+    while grade < 0 or grade > 100:
+        print("- - - - - - -")
+        print("Invalid grade! Try again and make sure grade values are between 0 and 100!")
+        print("- - - - - - -")
+        grade = float(input(f"Enter grade for module {i + 1}: "))
     grades_list.append(grade)
+
+print("- - - - - - -")
 print("You entered: ", grades_list)
-
-
-
-
-
-
-
-'''
-#Get info from user
-
-grade_m1 = float(input("Enter grade for module 1: ")) 
-grade_m2 = float(input("Enter grade for module 2: ")) 
-grade_m3 = float(input("Enter grade for module 3: "))
-grade_m4 = float(input("Enter grade for module 4: "))
-grade_m5 = float(input("Enter grade for module 5: "))
-grade_m6 = float(input("Enter grade for module 6: "))
-
-
-#Add values to list
-grades_list.append(grade_m1)
-grades_list.append(grade_m2)
-grades_list.append(grade_m3)
-grades_list.append(grade_m4)
-grades_list.append(grade_m5)
-grades_list.append(grade_m6)
+print("- - - - - - -")
 
 #Calculte grade stats
 print("------Results------")
@@ -57,5 +41,4 @@ print(f"Highest grade:   {max_grade:.1f}")
 print(f"Sum of grades:   {sum_grade:.1f}")
 print(f"Average:         {avg_grade:.2f}")
 
-print("-------------------")
-'''
+print("----------------------")
