7a2ed0b81f49e9f4a5b2c02c1191119e7a2bb6c4
isleym9447 <150169210+isleym9447@users.noreply.github.com>
2023-12-05 11:52:39 -0500

    Functions lab

---
+++ FunctionsLAB_IsleyMattea.py
@@ -0,0 +1,52 @@
+#Mattea Isley
+#11/5/23
+#User defined functions 
+
+#This fuction takes a bool parameter(true/false) and returns an integer
+def days_in_feb(is_leap):
+    if is_leap == True:
+        days = 29
+    else:                 #Represents is_leap = false
+        days = 28
+    return days
+
+
+
+#Create main function
+def main():
+    is_leap_year = False
+
+    input_year = int(input("Enter a year: "))
+
+    if (input_year % 4) == 0:      # inputYear is divisible by 4
+
+      if (input_year % 100) == 0:   # inputYear is divisible by 100 (century year)
+
+        if (input_year % 400) == 0: # inputYear is divisible by 400
+
+          is_leap_year = True
+
+        else:           # inputYear is not divisible by 400
+
+          is_leap_year = False
+
+      else:             # inputYear is not divisible by 100
+
+        is_leap_year = True
+
+    else:               # inputYear is not divisible by 4
+
+      is_leap_year = False
+
+      
+    #Call our function
+    num_days = days_in_feb(is_leap_year)
+    print(f"The year {input_year} has {num_days} days in febuary")
+
+
+
+
+#Call main function
+if __name__ == "__main__":
+    main()
+
