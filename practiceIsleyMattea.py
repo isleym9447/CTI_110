4c22fbfeb1d44a5755771009a291741eab048b31
isleym9447 <150169210+isleym9447@users.noreply.github.com>
2023-12-05 11:08:03 -0500

    Add files via upload

---
+++ practiceIsleyMattea.py
@@ -0,0 +1,37 @@
+#Mattea Isley
+#12/5/23
+#Loop practice
+
+
+'''
+(start, stop, step) (up to but not including the stop)
+(step is increment)
+arguements/parameters:
+range(10) 0-9
+range(0,11) 0-10
+range(0,13,2) 0,2,4,6,8,10,12
+
+print("HELLO"
+print("hello", num)
+print()   (0 paramaters)
+print(.) 1 parameter
+'''
+
+
+for bubblegum in range(0,8):
+    print(bubblegum)
+
+
+
+
+    
+
+run_again = "Y"
+
+while run_again == "Y":
+    print("Program is running...")
+    run_again = input("Want to run again? Y or N?: ")
+if run_again == "N":
+    print("Program has ended")
+else:
+    print("Program is running...")
